0958ce13bb6512ef68aff7be76b9654f493b0afe
Prathamesh Harad <97778106+Pratham421421@users.noreply.github.com>
2022-03-02 19:40:24 +0530

    Add files via upload

---
+++ February/week 4 (21-26 feb)/Assignment (22-2-2022)/even_or_odd.java
@@ -0,0 +1,17 @@
+package feb_22_Assignments;
+
+import java.util.Scanner;
+
+public class even_or_odd {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter number");
+		int n = scan.nextInt();
+		if(n%2==0) {
+			System.out.println("Even");
+		}
+		else {
+			System.out.println("Odd");
+		}
+	}
+}
